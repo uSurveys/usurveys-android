ff4b754eac2870c1eef417abe6153a840ca5f2b5
Allen Calderwood <calderwoodra1113@gmail.com>
2021-02-06 23:05:40 -0800

    Added stub instance and testing methods.

--- usersneak/src/main/java/com/usersneak/UserSneak.java
+++ usersneak/src/main/java/com/usersneak/UserSneak.java
@@ -1,10 +1,25 @@
 package com.usersneak;
 
+import androidx.annotation.VisibleForTesting;
 import com.usersneak_api.UserSneakApi;
 import com.usersneak_internal.UserSneakImpl;
 
 /** @see UserSneakApi for setup/usage instructions. */
 public final class UserSneak {
 
-  public static final UserSneakApi INSTANCE = new UserSneakImpl();
+  private static UserSneakApi INSTANCE = new UserSneakImpl();
+
+  public static UserSneakApi get() {
+    return INSTANCE;
+  }
+
+  @VisibleForTesting
+  public static void setForTesting(UserSneakApi userSneak) {
+    INSTANCE = userSneak;
+  }
+
+  @VisibleForTesting
+  public static void setStubInstance() {
+    INSTANCE = new UserSneakStub();
+  }
 }
---
+++ usersneak/src/main/java/com/usersneak/UserSneakStub.java
@@ -0,0 +1,63 @@
+package com.usersneak;
+
+import android.content.Context;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import com.usersneak_api.SurveyResultsHandler;
+import com.usersneak_api.UserSneakApi;
+import java.util.ArrayList;
+
+public final class UserSneakStub implements UserSneakApi {
+
+  @Override
+  public UserSneakApi configureUserSneakApiKey(Context context, String apiKey) {
+    return this;
+  }
+
+  @Override
+  public UserSneakApi configureSheetsApi(Context context, String sheetsApiKey, String sheetId) {
+    return this;
+  }
+
+  @Override
+  public UserSneakApi configureSurveyResultsHandler(Context context, SurveyResultsHandler handler) {
+    return this;
+  }
+
+  @Override
+  public UserSneakApi configureResurveyWindowMillis(long millis) {
+    return this;
+  }
+
+  @Override
+  public void preTrack(String event) {}
+
+  @Override
+  public void track(String event, StatusCallback statusCallback) {
+    statusCallback.handleStatus(StatusCallback.SurveyStatus.AVAILABLE);
+  }
+
+  @Override
+  public void showSurvey(
+      FragmentActivity activity, ActivityResultCallback<ActivityResult> resultCallback) {
+    resultCallback.onActivityResult(new ActivityResult(AppCompatActivity.RESULT_OK, null));
+  }
+
+  @Override
+  public void logout(boolean clearResurveyWindow) {}
+
+  @Override
+  public void showTestSurvey(
+      FragmentActivity activity, ActivityResultCallback<ActivityResult> resultCallback) {}
+
+  @Override
+  public void getAllEvents(AllEventsCallback callback) {
+    ArrayList<String> events = new ArrayList<>();
+    events.add("App First Launch");
+    events.add("Onboarding Complete");
+    events.add("User Achieved Milestone");
+    callback.handleEventsReady(events);
+  }
+}
